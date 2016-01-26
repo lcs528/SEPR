2da392a9057d8727e19a6374e2f03081ea40ac5b
ramzizzle <cr902@york.ac.uk>
2016-01-26 19:41:27 +0000

    Game can end
    returns to home screen

--- Assets/Scripts/menuButtons.cs
+++ Assets/Scripts/menuButtons.cs
@@ -7,7 +7,10 @@ using System.Collections;
 /// </summary>
 
 public class menuButtons : MonoBehaviour {
-	
+	void Start() {		
+		Cursor.lockState = CursorLockMode.None;
+		Cursor.visible = true;
+	}
 	public void pressPlay () {
 		Application.LoadLevel (1);
 	}
